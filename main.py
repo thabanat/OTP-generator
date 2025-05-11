88b8e9c5b13a50ee80884b47155143fb1a88ca35
Tumelo Thabane <131687797+thabanat@users.noreply.github.com>
2025-05-11 18:23:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import secrets
 
-def generate_otp(length=6):
-    return ''.join(str(secrets.randbelow(10))
-                   for _ in range(length))
-    
-print("Generated OTP:", generate_otp())
